3716d16797067962d1d311aeec7a6e360648ec56
Andrei Golubenko <95508920+agolubenk@users.noreply.github.com>
2023-01-15 21:46:43 +0300

    21:45 15 jan 23
    changed db from sqlight to postgresql
    deleted all migrations

--- want2it/settings.py
+++ want2it/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'want2it.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': '2798',
     }
 }
 
